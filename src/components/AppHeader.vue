7f9f2b50279a5c36cdd36343cf3c52081c2c18de
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-11 17:50:49 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        name: 'AppHeader',
+    }
+</script>
+
+<template>
+    <header>
+        <h1>Ciao mondo</h1>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
