5da7ba0f044aef4d356d8e47aa3c8e026be95c64
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-14 22:46:56 +0200

    feat: add the props in the 'AppCardListing' and generate the entire list using the v-for in the parent component and use the variables created in the props to show the exact data that we need

--- src/components/AppCardListing.vue
+++ src/components/AppCardListing.vue
@@ -2,25 +2,12 @@
 
 export default {
     name: 'AppCardListing',
-
-    data() {
-        return {
-            comics: [
-                {
-                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
-                    "price": "$19.99",
-                    "series": "Action Comics",
-                    "type": "comic book"
-                },
-                {
-                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
-                    "price": "$3.99",
-                    "series": "American Vampire 1976",
-                    "type": "comic book"
-                },
-                ]
-            }         
-        }
+    props: {
+        image: String,
+        title: String
+    }
+        
+    
 }
 
 </script>
@@ -28,32 +15,8 @@ export default {
 <template>
 
     <div class="comic-card">
-        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-        <h4>Action Comics</h4>
-    </div>
-    <div class="comic-card">
-        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-        <h4>Action Comics</h4>
-    </div>
-    <div class="comic-card">
-        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-        <h4>Action Comics</h4>
-    </div>
-    <div class="comic-card">
-        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-        <h4>Action Comics</h4>
-    </div>
-    <div class="comic-card">
-        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-        <h4>Action Comics</h4>
-    </div>
-    <div class="comic-card">
-        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-        <h4>Action Comics</h4>
-    </div>
-    <div class="comic-card">
-        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-        <h4>Action Comics</h4>
+        <img :src="image" alt="">
+        <h4>{{ title }}</h4>
     </div>
 
 </template>
@@ -70,8 +33,10 @@ export default {
         cursor: pointer;
 
         img {
-            max-width: 100%;
+            width: 180px;
+            height: 180px;
             object-fit: cover;
+            object-position:top
         }
 
         h4 {
